b900b698bee62c26106bd5340331f3e3723381cb
OlliMartin <scholl3ss93@gmail.com>
2025-02-11 06:43:53 +0100

    I dont understand anything

--- acaad.testing/src/index.ts
+++ acaad.testing/src/index.ts
@@ -1,3 +1,7 @@
+import { AcaadApiServer, AcaadSignalRServer, IAcaadApiServer, IAcaadSignalRServer } from './api';
+import { AcaadAuthentication, AcaadHost } from '@acaad/abstractions';
+import { v4 as uuidv4 } from 'uuid';
+
 export {
   AcaadApiServer,
   IAcaadApiServer,
@@ -6,3 +10,41 @@ export {
   IEventReceiver
 } from './api';
 export { delay } from './utility';
+
+export class ServerMocks {
+  serverName: string;
+  apiServer: IAcaadApiServer;
+  signalrServer: IAcaadSignalRServer;
+
+  constructor(apiServer: IAcaadApiServer, signalrServer: IAcaadSignalRServer) {
+    this.serverName = `mock-${uuidv4()}`;
+
+    this.apiServer = apiServer;
+    this.signalrServer = signalrServer;
+  }
+
+  static async createMockServersAsync(
+    selectedCollection = 'positive',
+    componentCount: number = 1
+  ): Promise<ServerMocks> {
+    const [apiServer, signalrServer] = await Promise.all([
+      AcaadApiServer.createMockServerAsync(selectedCollection, componentCount),
+      AcaadSignalRServer.createMockServerAsync()
+    ]);
+
+    return new ServerMocks(apiServer, signalrServer);
+  }
+
+  getHost(): AcaadHost {
+    const auth = new AcaadAuthentication('', '', '', []);
+    return new AcaadHost(this.serverName, 'localhost', this.apiServer.port, auth, this.signalrServer.port);
+  }
+
+  public async startAsync() {
+    return await Promise.all([this.apiServer.startAsync(), this.signalrServer.startAsync()]);
+  }
+
+  public async disposeAsync() {
+    return await Promise.all([this.apiServer.disposeAsync(), this.signalrServer.disposeAsync()]);
+  }
+}
